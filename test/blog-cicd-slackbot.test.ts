0425f1caca4eda9e602254c2438d18ea173ceaf6
martzcodes <978362+martzcodes@users.noreply.github.com>
2023-08-17 10:57:00 -0400

    Initial commit

---
+++ test/blog-cicd-slackbot.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as BlogCicdSlackbot from '../lib/blog-cicd-slackbot-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/blog-cicd-slackbot-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new BlogCicdSlackbot.BlogCicdSlackbotStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
